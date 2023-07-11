59a8284989e8dbb0989b2b4ef81297b70435c187
Bayarto Neltanov <bayarto2003@gmail.com>
2023-07-11 19:14:17 +0700

    Multiplexing of I/O was done

--- proxy.c
+++ proxy.c
@@ -25,22 +25,27 @@ void
 handle_client(int server_socket, int client_socket)
 {
     char buffer[BUFFER_SIZE];
-
-    /* Recieving data from client */
-    int bytes_received;
-    while ((bytes_received = recv(client_socket, buffer, BUFFER_SIZE, 0)) > 0)
-    {
-        /* add to server_log which data received */
-        /* Sending reply to the client */
-        if (send(server_socket, buffer, bytes_received, 0) == -1)
+    int bytes_recieved = recv(client_socket, buffer, BUFFER_SIZE - 1, 0);
+    if (bytes_recieved <= 0) {
+        if (bytes_recieved == 0)
         {
-            /* add error to proxy_log */
-            perror("Client data sending error");
-            close(client_socket);
+            printf("Client has lost connection\n");
+        } else
+        {
+            perror("Data reading error");
+        }
+
+        close(client_socket);
+    } else
+    {
+        buffer[bytes_recieved] = '\0';
+        printf("Recieved from client: %s\n", buffer);
+        
+        if (send(client_socket, "The message was recieved", 18, 0) == -1)
+        {
+            perror("Data sending error");
         }
     }
-
-    /* doing something with the buffer */
 }
 
 int 
@@ -62,9 +67,22 @@ connect_postgres_server()
  *  Rethink _exit() in "ifs"
  */
 
+void
+accept_connection(int client_socket, int server_socket, struct sockaddr_in *client_address) 
+{
+    socklen_t client_len = sizeof(client_address);
+    client_socket = accept(server_socket, (struct sockaddr *)&client_address, &client_len);
+    if (client_socket == -1)
+    {
+        perror("Connection accept error");
+        exit(1);
+    }
+}
+
 void 
 run_proxy()
 {
+    /* TODO: create client and server structure for avoiding too many params in functions */
     int server_socket, client_socket;
     struct sockaddr_in server_address, client_address;
     int client_address_size = sizeof(client_address);
@@ -105,29 +123,50 @@ run_proxy()
     /* do we really need it --- otherwise, add it in the proxy_log */
     printf("The proxy server is running. Waiting for connections...\n");
 
-    /* Server loop */
-    for (;;) 
+    /* 
+     * We need to use two fd sets because select() systemcall modify read_fds
+     * and we must update this fd set in each iteration
+     */
+    fd_set master_fds, read_fds;
+    /* For first argument of select() */
+    int max_fd;
+
+    FD_ZERO(&master_fds);
+    FD_SET(server_socket, &master_fds);
+    max_fd = server_socket;
+    char buffer[BUFFER_SIZE];
+    /* Accepting connections and handling clients */
+    while(1)
     {
-        /* Accepting connections */
-        client_socket = accept(server_socket, (struct sockaddr *)&client_address, (socklen_t *)&client_address_size);
-        if (client_socket == -1)
+        read_fds = master_fds;
+        
+        if (select(max_fd + 1, &read_fds, NULL, NULL, NULL) == -1)
         {
-            /* add error to proxy_log */
-            perror("Connection accepting error");
+            perror("Select calling error");
             exit(1);
-        } 
-        
-        /* do we really need it --- otherwise, add it in the proxy_log */
-        printf("Connection accepted.\n");
-        
-        handle_client(server_socket, client_socket);
+        }
 
-        /* I think it is more reasonable to let server close client_socket in loop 
-         * 'cauz something can go wrong in that function -- > socket wom't be closed 
-         */
-        close(client_socket);
-
-        hand_data_to_postgres_server();
+        for (int fd = 0; fd <= max_fd; fd++)
+        {
+            if (FD_ISSET(fd, &read_fds))
+            {
+                if (fd == server_socket)
+                {
+                    accept_connection(client_socket, server_socket, &client_address);
+                    if (client_socket > max_fd)
+                    {
+                        max_fd = client_socket;
+                    }
+                    FD_SET(client_socket, &master_fds);
+                    elog(LOG, "New connection from client: %s:%d\n",
+                            inet_ntoa(client_address.sin_addr), ntohs(client_address.sin_port));
+                }
+                else {
+                    handle_client(server_socket, client_socket);
+                    FD_CLR(fd, &master_fds);
+                }
+            }
+        }
     }
 
     close(server_socket);
